25caf56af029bf3bf2d5b286e64831b9f6a827f6
callistachang <call0001@e.ntu.edu.sg>
2019-04-28 23:06:11 +0800

    Update BCPL Airdrop Form Filler.py

--- BCPL Airdrop Form Filler.py
+++ BCPL Airdrop Form Filler.py
@@ -5,24 +5,35 @@ from time import sleep
 pyautogui.PAUSE = 0.8
 pyautogui.FAILSAFE = True
 
-#coordinates & rgb values
-blueSend = (127, 87)
-addressBox = (710, 216)
-pickSGPay = (715, 303)
-sendParticulars = (77, 558)
-okButton = (760, 463)
-clickMetamask = (1251, 47)
-gasClickFrom = (1184, 306)
-gasDragTo = (1193, 306)
-greenSubmit = (1110, 440)
-clickWallet = (73, 89)
-blueSendColor = (133, 192, 218)
-gasColor = (236, 236, 236)
-greenSubmitColor = (106, 195, 96)
+class Coord:
+    send1 = (127, 87)
+    addressbox = (710, 216)
+    sgp = (715, 303)
+    send2 = (77, 558)
+    ok = (760, 463)
+    metamask = (1251, 47)
+    gasfrom = (1184, 306)
+    gasto = (1193, 306)
+    submit = (1110, 440)
+    wallet = (1110, 440)
 
+class Color:
+    send1 = (133, 192, 218)
+    gasfrom = (236, 236, 236)
+    submit = (106, 195, 96)
+    
+def click(coordinates):
+    pyautogui.click(coordinates[0], coordinates[1])
+    
+def matchcolor(coordinates, color):
+    pyautogui.pixelMatchesColor(coordinates[0], coordinates[1], color)
+    
+def drag(coordinates):
+    pyautogui.dragTo(coordinates[0], coordinates[1], duration = 0.5)
+    
 #excel data to list
 li = []
-sheet = openpyxl.load_workbook('facebookairdrop.xlsx')['Sheet1']
+sheet = openpyxl.load_workbook('twitterairdrop.xlsx')['Sheet1']
 for i in range(100):
     d = {}
     x = 0 #update to row you wish to start iterating from
@@ -30,35 +41,30 @@ for i in range(100):
     d['address'] = sheet.cell(row=i+x, column=3).value
     d['tokens'] = sheet.cell(row=i+x, column=12).value
     li.append(d)
-
-#the program!
-print('WE HAVE BEGUN')
+    
+print("Program started!")
 for person in li:
-    while not pyautogui.pixelMatchesColor(blueSend[0], blueSend[1], blueSendColor):
+    while not matchcolor(Coord.send1, Color.send1):
         sleep(0.5)
-    pyautogui.click(blueSend[0], blueSend[1])
-    print("Entering %s's info..." % (person['user']))
-    pyautogui.click(addressBox[0], addressBox[1])
-    pyautogui.typewrite(person['address'] + '\t' + str(person['tokens']))
-    pyautogui.click(pickSGPay[0], pickSGPay[1])
-    sleep(2)
-    pyautogui.click(sendParticulars[0], sendParticulars[1])
+    click(Coord.send1)
+    print(f"Entering {person['user']}'s info...")
+    click(Coord.addressbox)
+    pyautogui.typewrite(person["address"] + "\t" + str(person["tokens"]))
+    click(Coord.sgp)
     sleep(1)
-    pyautogui.click(okButton[0], okButton[1])
+    click(Coord.send2)
     sleep(1)
-    pyautogui.click(clickMetamask[0], clickMetamask[1])
-    '''
-    while not pyautogui.pixelMatchesColor(gasClickFrom[0], gasClickFrom[1], gasColor):
+    click(Coord.ok)
+    sleep(1)
+    click(Coord.metamask)
+    sleep(1)
+    while not matchcolor(Coord.gasfrom, Color.gasfrom):
         sleep(0.5)
-    sleep(1)
-    pyautogui.click(gasClickFrom[0], gasClickFrom[1])
-    pyautogui.dragTo(gasDragTo[0], gasDragTo[1], duration=0.5)
-    pyautogui.typewrite('5')
-    '''
-    while not pyautogui.pixelMatchesColor(greenSubmit[0], greenSubmit[1], greenSubmitColor):
+    click(Coord.gasfrom)
+    drag(Coord.gasto)
+    pyautogui.typewrite("5")    #common gas price
+    while not matchcolor(Coord.submit, Color.submit):
         sleep(0.5)
+    click(Coord.submit)
     sleep(1)
-    pyautogui.click(greenSubmit[0], greenSubmit[1])
-    sleep(1)
-    pyautogui.click(clickWallet[0], clickWallet[1])
-    sleep(30)
+    click(Coord.wallet)
